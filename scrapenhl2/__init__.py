aef05632b7fd81ef44a9dd544b4323235fad074d
Muneeb Alam <muneeb.alam@gmail.com>
2017-10-09 08:59:59 -0400

    Deleted scrapenhl2.__init__.py

--- scrapenhl2/__init__.py
+++ scrapenhl2/__init__.py
@@ -1,9 +1,3 @@
-"""
-At import, this module creates folders for data storage if need be.
-
-It also creates a team ID mapping and schedule files from 2005 through the current season (if the files do not exist).
-"""
-
 import scrapenhl2.scrape.scrape_setup
 import scrapenhl2.scrape.scrape_game
-import scrapenhl2.scrape.scrape_season
+import scrapenhl2.manipulate.manipulate
