fc53b1736842e65b822b4dd43aba5b88494b95e7
Muneeb Alam <muneeb.alam@gmail.com>
2017-10-21 22:35:04 -0400

    Added player pages

--- scrapenhl2/plot/app/__init__.py
+++ scrapenhl2/plot/app/__init__.py
@@ -4,7 +4,9 @@ from io import BytesIO
 from flask import Flask, render_template, make_response
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
-import scrapenhl2
+import scrapenhl2.plot.visualize_game as vg
+import scrapenhl2.scrape.scrape_game as sg
+import scrapenhl2.scrape.scrape_setup as ss
 
 app = Flask(__name__)
 
@@ -14,11 +16,24 @@ def index():
     return render_template('index.html')
 
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
+
 @app.route('/update/')
 def update_data():
     # TODO add streaming output
     try:
-        scrapenhl2.autoupdate()
+        sg.autoupdate()
         return render_template('updatepage.html', msg='Done updating data')
     except Exception as e:
         return render_template('updatepage.html', msg=str(e) + str(e.args))
@@ -27,14 +42,14 @@ def update_data():
 @app.route('/games/<int:season>/')
 @app.route('/games/<int:season>/<team>/')
 def get_game_list(season, team=None):
-    sch = scrapenhl2.get_season_schedule(season)
+    sch = ss.get_season_schedule(season)
 
     if team is not None:
-        tid = scrapenhl2.team_as_id(team)
+        tid = ss.team_as_id(team)
         sch = sch[(sch.Home == tid) | (sch.Road == tid)]
 
-    sch.loc[:, 'Home'] = sch.Home.apply(lambda x: scrapenhl2.team_as_str(x))
-    sch.loc[:, 'Road'] = sch.Road.apply(lambda x: scrapenhl2.team_as_str(x))
+    sch.loc[:, 'Home'] = sch.Home.apply(lambda x: ss.team_as_str(x))
+    sch.loc[:, 'Road'] = sch.Road.apply(lambda x: ss.team_as_str(x))
     sch = sch[['Date', 'Game', 'Home', 'Road', 'HomeScore', 'RoadScore', 'Status', 'Result']] \
         .rename(columns={'Result': 'HomeResult'})
     sch.loc[:, 'H2H'] = sch.Game.apply(lambda x: '/games/{0:d}/h2h/{1:d}/'.format(season, x))
@@ -43,13 +58,36 @@ def get_game_list(season, team=None):
     return render_template('games.html', season=season, schedule=sch)
 
 
+@app.route('/players/')
+@app.route('/players/<pfilter>')
+def get_player_list(pfilter=None):
+    pinfo = ss.get_player_ids_file()
+
+    if pfilter is None:
+        pinfo = pinfo.sort_values(by=['Name', 'DOB'])
+    else:
+        pinfo = ss.add_sim_scores(pinfo, pfilter)
+        pinfo = pinfo.sort_values(by=['SimScore', 'DOB'], ascending=False)
+        pinfo = pinfo.iloc[:50, :]  # limit size
+
+    pinfo.loc[:, 'Link'] = pinfo.ID.apply(lambda x: '/player/{0:d}'.format(int(x)))
+    pinfo = pinfo[['DOB', 'Hand', 'Name', 'Pos', 'Height', 'Weight', 'Link']]
+    return render_template('players.html', pinfo=pinfo)
+
+
+@app.route('/player/<int:pid>/')
+def get_player_links(pid):
+    pname = ss.player_as_str(pid)
+    return render_template('individual.html', pname=pname)
+
+
 @app.route('/games/<int:season>/<charttype>/<int:game>/')
 def graph(season, game, charttype):
     # TODO get these working in templates!
     if charttype == 'h2h':
-        fig = scrapenhl2.game_h2h(season, game, "fig")
+        fig = vg.game_h2h(season, game, "fig")
     elif charttype == 'tl':
-        fig = scrapenhl2.game_timeline(season, game, "fig")
+        fig = vg.game_timeline(season, game, "fig")
         pass
     else:
         return render_template("chart.html", message="Invalid chart type; use h2h or tl")
