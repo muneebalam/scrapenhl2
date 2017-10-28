992a00288827484ebf4d8b9a2169fecdc64a3070
Muneeb Alam <muneeb.alam@gmail.com>
2017-10-28 11:11:34 -0400

    organization.py for folders

---
+++ scrapenhl2/scrape/organization.py
@@ -0,0 +1,110 @@
+"""
+This module contains paths to folders.
+"""
+
+import os.path
+
+
+def check_create_folder(*args):
+    """
+    A helper method to create a folder if it doesn't exist already
+    :param args: the parts of the filepath. These are joined together with the base directory
+    :return: nothing
+    """
+    path = os.path.join(get_base_dir(), *args)
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def get_base_dir():
+    """
+    Returns the base directory of this package (one directory up from this file)
+    :return: the base directory
+    """
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+
+def get_raw_data_folder():
+    """
+    Returns the folder containing raw data
+    :return: /scrape/data/raw/
+    """
+    return os.path.join(get_base_dir(), 'data', 'raw')
+
+
+def get_parsed_data_folder():
+    """
+    Returns the folder containing parsed data
+    :return: /scrape/data/parsed/
+    """
+    return os.path.join(get_base_dir(), 'data', 'parsed')
+
+
+def get_team_data_folder():
+    """
+    Returns the folder containing team log data
+    :return: /scrape/data/teams/
+    """
+    return os.path.join(get_base_dir(), 'data', 'teams')
+
+
+def get_other_data_folder():
+    """
+    Returns the folder containing other data
+    :return: /scrape/data/other/
+    """
+    return os.path.join(get_base_dir(), 'data', 'other')
+
+
+def get_season_raw_pbp_folder(season):
+    """
+    Returns the folder containing raw pbp for given season
+    :param season: int, current season
+    :return: /scrape/data/raw/pbp/[season]/
+    """
+    return os.path.join(get_raw_data_folder(), 'pbp', str(season))
+
+
+def get_season_raw_toi_folder(season):
+    """
+    Returns the folder containing raw toi for given season
+    :param season: int, current season
+    :return: /scrape/data/raw/toi/[season]/
+    """
+    return os.path.join(get_raw_data_folder(), 'toi', str(season))
+
+
+def get_season_parsed_pbp_folder(season):
+    """
+    Returns the folder containing parsed pbp for given season
+    :param season: int, current season
+    :return: /scrape/data/parsed/pbp/[season]/
+    """
+    return os.path.join(get_parsed_data_folder(), 'pbp', str(season))
+
+
+def get_season_parsed_toi_folder(season):
+    """
+    Returns the folder containing parsed toi for given season
+    :param season: int, current season
+    :return: /scrape/data/raw/toi/[season]/
+    """
+    return os.path.join(get_parsed_data_folder(), 'toi', str(season))
+
+
+def get_season_team_pbp_folder(season):
+    """
+    Returns the folder containing team pbp logs for given season
+    :param season: int, current season
+    :return: /scrape/data/teams/pbp/[season]/
+    """
+    return os.path.join(get_team_data_folder(), 'pbp', str(season))
+
+
+def get_season_team_toi_folder(season):
+    """
+    Returns the folder containing team toi logs for given season
+    :param season: int, current season
+    :return: /scrape/data/teams/toi/[season]/
+    """
+    return os.path.join(get_team_data_folder(), 'toi', str(season))
