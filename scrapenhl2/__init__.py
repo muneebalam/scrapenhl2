3d7bf61fd640de6ba30b549c995c4fe2d4da96f3
Muneeb Alam <muneeb.alam@gmail.com>
2017-10-28 17:47:10 -0400

    Changed init for package

--- scrapenhl2/__init__.py
+++ scrapenhl2/__init__.py
@@ -1,6 +1,1 @@
-from scrapenhl2.manipulate.manipulate import *
-from scrapenhl2.plot.visualize_app import *
-from scrapenhl2.plot.visualize_game import *
-from scrapenhl2.plot.visualize_player import *
-from scrapenhl2.scrape.scrape_game import *
-from scrapenhl2.scrape.scrape_setup import *
+__all__ = ['scrape', 'manipulate', 'plot']
