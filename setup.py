792b1ae53d8e42766b40d5b3270c42444c7adfeb
Muneeb Alam <muneeb.alam@gmail.com>
2017-11-08 20:15:56 -0500

    updated to 0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setuptools.setup(
     name="scrapenhl2",
-    version="0.3.9",
+    version="0.4.00",
     author="Muneeb Alam",
     author_email="muneeb.alam@gmail.com",
     description=("scrapenhl2 is a python package for scraping and manipulating NHL data pulled from the NHL website."),
